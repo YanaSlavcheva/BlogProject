77cb652348ddb59c0d9af833b114e07646722d1b
YanaSlavcheva <yana.slavcheva@gmail.com>
2016-06-20 20:47:48 +0300

    Added Data, Services, Web, Tests folders and Projects arranged in them (Web, Tests)

---
+++ BlogProject/ViewModels/Account/ExternalLoginListViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace BlogProject.ViewModels.Account
+{
+    public class ExternalLoginListViewModel
+    {
+        public string ReturnUrl { get; set; }
+    }
+}
---
+++ BlogProject/ViewModels/Manage/FactorViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace BlogProject.ViewModels.Manage
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
